f63e649bfa6a71257217effef42d3a3f5c0374ad
SwethaGudla <81808738+SwethaGudla@users.noreply.github.com>
2021-05-27 12:37:28 +0530

    Update Songs_POC.py

--- Songs_POC.py
+++ Songs_POC.py
@@ -5,31 +5,16 @@ while True:
         print(i+1,song_list[i][:-4])
     try:
         song_name = int(input('enter you choice: '))
-    
     except:
          print('invalid')
          break
-    
-    try:
-        
-         
+    try: 
         if song_list[song_name-1] in song_list:
             f=open(f'songslyrics\{song_list[song_name-1]}','r')
             print(f.read())
-            f.close()
-        
+            f.close()    
     except:
         print('Invalid option\make correct choice')
-        
-
     x=input('press * to choose again')
     if '*' != x:
         break
-
-
-
-
-
-
-
-
